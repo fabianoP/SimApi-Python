f639950501236532fd24b30e3a4ea92d28d2df45
RichieBrady <richard.brady@ucdconnect.ie>
2020-01-28 11:11:53 +0000

    Created RestApi models

--- src/simapi_web/rest_api/models.py
+++ src/simapi_web/rest_api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Timestep(models.Model):
+    """Represents the Timestep values for the fmu"""
+    user_id = models.IntegerField()
+    time_step = models.BigIntegerField()
+
+
+class Output(models.Model):
+    """Represents output received from an fmu timestep"""
+
+    time_step = models.BigIntegerField()
+    yshade = models.DecimalField(max_digits=20, decimal_places=4)
+    dry_bulb = models.DecimalField(max_digits=20, decimal_places=4)
+    troo = models.DecimalField(max_digits=20, decimal_places=4)
+    isolext = models.DecimalField(max_digits=20, decimal_places=4)
+    sout = models.DecimalField(max_digits=20, decimal_places=4)
+    zonesens = models.DecimalField(max_digits=20, decimal_places=4)
+    cool_rate = models.DecimalField(max_digits=20, decimal_places=4)
+
+
+class Input(models.Model):
+    """Represents fmu inputs from web api"""
+
+    time_step = models.BigIntegerField()
+    yshade = models.DecimalField(max_digits=20, decimal_places=4)
+
+
