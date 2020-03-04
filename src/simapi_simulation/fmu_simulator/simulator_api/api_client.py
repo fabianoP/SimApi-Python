ce847ba74b137c7bb595a130f4352791f28f30a8
RichieBrady <richard.brady@ucdconnect.ie>
2020-03-04 10:58:37 +0000

    client api

--- src/simapi_simulation/fmu_simulator/simulator_api/api_client.py
+++ src/simapi_simulation/fmu_simulator/simulator_api/api_client.py
@@ -3,28 +3,17 @@ import requests
 
 # TODO implement api client here. Receive input, provide output.
 
-#resp = requests.get('http://web:8000/')
-#print(resp)
+# resp = requests.get('http://web:8000/')
+# print(resp)
+data1 = {"username": "test@test.com",
+         "password": "hello world88"}
 
-resp_2 = requests.get('http://web:8000/')
-print(resp_2)
+resp_2 = requests.post('http://127.0.0.1:8000/login/', data=data1)
+print(resp_2.headers)
+print(resp_2.cookies)
+print(resp_2.content)
 
-# client = coreapi.Client()
-# scheme = client.get('http://web:8000/')
+data = resp_2.json
 
-# action = ['login', 'post']
+print(data)
 
-# params = {"username": "test@test.com", "password": "hello world88"}
-
-# result = client.action(scheme, action, params)
-
-# auth = coreapi.auth.TokenAuthentication(
-#    scheme='JWT',
-#    token=result['token']
-# )
-# client = coreapi.Client(auth=auth)
-
-# new_init = client.action(scheme, ['init_model', 'create'], params={"model_name": "simulator",
-#                                                                   "step_size": "600",
-#                                                                  "final_time": "72.0"})
-# print(new_init)
